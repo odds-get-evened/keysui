51900f43b3bdc5df46a6419ed53e472e49f9138d
Chris Walsh <chris.is.rad@pm.me>
2024-10-19 19:29:45 -0400

    key generation. clean up window closing error

--- ui.py
+++ ui.py
@@ -1,6 +1,15 @@
+import dbm
+import hashlib
+import os.path
+import re
 import sys
 import tkinter as tk
-from tkinter import ttk
+import warnings
+from pathlib import Path
+from tkinter import ttk, filedialog, messagebox
+
+import pgpy
+from pgpy.constants import PubKeyAlgorithm, KeyFlags, HashAlgorithm, SymmetricKeyAlgorithm, CompressionAlgorithm
 
 
 class PGPKeys:
@@ -8,8 +17,54 @@ class PGPKeys:
     PRIV = 'private'
 
 
+class KeyStuff:
+    @classmethod
+    def generate_key(cls, name, email, comment='', protect=False):
+        warnings.filterwarnings('ignore')  # because PGPy dev put warnings in code print
+        key = pgpy.PGPKey.new(PubKeyAlgorithm.RSAEncryptOrSign, 4096)
+        uid = pgpy.PGPUID.new(name.strip(), comment=comment, email=email)
+        key.add_uid(
+            uid,
+            usage={KeyFlags.Sign, KeyFlags.EncryptCommunications, KeyFlags.EncryptStorage},
+            hashes=[HashAlgorithm.SHA256, HashAlgorithm.SHA512],
+            ciphers=[SymmetricKeyAlgorithm.AES256],
+            compression=[
+                CompressionAlgorithm.ZLIB,
+                CompressionAlgorithm.BZ2,
+                CompressionAlgorithm.ZIP
+            ]
+        )
+
+        if protect:
+            # use hashed comment as password
+            passwd = hashlib.sha256(comment.strip().encode()).hexdigest()
+            key.protect(passwd, SymmetricKeyAlgorithm.AES256, HashAlgorithm.SHA256)
+
+        return key
+
+
+class DBStuff:
+    DB_PATH = Path(os.path.expanduser('~'), '.databox', 'keysui', 'storage')
+
+    @classmethod
+    def check(cls):
+        if not cls.DB_PATH.parent.exists():
+            cls.DB_PATH.parent.mkdir(parents=True, exist_ok=True)
+
+    @classmethod
+    def insert_key(cls, key: pgpy.PGPKey):
+        cls.check()
+        with dbm.open(cls.DB_PATH.__str__(), 'c') as db:
+            h = hashlib.sha256().hexdigest()
+            db[h] = key.__str__()
+        print(f"key [{h}] successfully stored.")
+
+
 class AppWin:
+    PAD5 = 5
+
     def __init__(self):
+        self.gen_ok_btn = None
         self.gen_pop = None
         self.confirm_gen_btn: ttk.Button = None
         self.style: ttk.Style = None
@@ -17,9 +72,9 @@ class AppWin:
         self.upload_btn = None
         self.gen_btn = None
         self.r_frame = None
-        self.cmnt_entry = None
+        self.cmnt_txt = None
         self.cmnt_lbl = None
-        self.email_entry = None
+        self.email_txt = None
         self.email_lbl = None
         self.name_txt = None
         self.name_lbl = None
@@ -56,53 +111,82 @@ class AppWin:
         self.gen_btn = ttk.Button(self.r_frame, text="generate key", command=self.open_gen_win)
         self.gen_btn.grid(row=0, column=0)
 
-        self.upload_btn = ttk.Menubutton(self.r_frame, text="upload...")
-        self.upload_menu = tk.Menu(self.upload_btn, tearoff=0)
-        self.upload_menu.add_command(label="public key", command=lambda: self.upload_key())
-        self.upload_menu.add_command(label="private key", command=lambda: self.upload_key(option=PGPKeys.PRIV))
-        self.upload_btn.config(menu=self.upload_menu)
-        self.upload_btn.grid(row=1, column=0, pady=5)
-
     def open_gen_win(self):
         self.gen_pop = tk.Toplevel(self.win)
         self.gen_pop.title('key generation tool')
         w = 340
         h = 180
-        x_pos = int((self.win.winfo_screenwidth() / 2) - (w / 2)) + self.win.winfo_x()
-        y_pos = int((self.win.winfo_screenheight() / 2) - (h / 2)) + self.win.winfo_y()
+        self.gen_pop.minsize(w, h)
+        x_pos = int(self.win.winfo_x() + (w / 2))
+        y_pos = int(self.win.winfo_y() + (h / 2))
         self.gen_pop.geometry(f"{w}x{h}+{x_pos}+{y_pos}")
 
-        self.name_lbl = ttk.Label(self.gen_pop, text='name')
-        self.name_lbl.grid(row=0, column=0)
+        c = ttk.Frame(self.gen_pop)
+        c.grid(sticky='nsew', padx=10, pady=10)
 
-        self.name_txt = ttk.Entry(self.gen_pop, textvariable=self.name_var)
-        self.name_txt.grid(row=0, column=1)
+        self.name_lbl = ttk.Label(c, text='name', style='C.TLabel')
+        self.name_lbl.grid(row=0, column=0, pady=AppWin.PAD5)
 
-        self.email_lbl = ttk.Label(self.gen_pop, text='email')
-        self.email_lbl.grid(row=1, column=0)
+        bb = ttk.Frame(c)
+        bb.grid(row=3, column=0, sticky='ew', columnspan=2, pady=AppWin.PAD5)
+        self.gen_ok_btn = ttk.Button(bb, text='ok', command=self.proc_gen_key, state=tk.DISABLED)
 
-        self.email_entry = ttk.Entry(self.gen_pop, textvariable=self.email_var)
-        self.email_entry.grid(row=1, column=1)
+        self.name_txt = ttk.Entry(c, textvariable=self.name_var)
+        self.name_txt.bind('<KeyRelease>', self.validate_gen_key)
+        self.name_txt.grid(row=0, column=1, sticky='ew', pady=AppWin.PAD5)
 
-        self.cmnt_lbl = ttk.Label(self.gen_pop, text='comments')
-        self.cmnt_lbl.grid(row=2, column=0)
+        self.email_lbl = ttk.Label(c, text='email')
+        self.email_lbl.grid(row=1, column=0, pady=5)
 
-        self.cmnt_entry = ttk.Entry(self.gen_pop, textvariable=self.cmnt_var)
-        self.cmnt_entry.grid(row=2, column=1)
+        self.email_txt = ttk.Entry(c, textvariable=self.email_var)
+        self.email_txt.bind('<KeyRelease>', self.validate_gen_key)
+        self.email_txt.grid(row=1, column=1, sticky='ew', pady=AppWin.PAD5)
 
-        ok = ttk.Button(self.gen_pop, text='ok', command=self.proc_gen_key, state=tk.DISABLED)
-        ok.grid(row=3, column=0)
-        cancel = ttk.Button(self.gen_pop, text='cancel', command=self.gen_pop.destroy)
-        cancel.grid(row=3, column=1)
+        self.cmnt_lbl = ttk.Label(c, text='comments')
+        self.cmnt_lbl.grid(row=2, column=0, pady=AppWin.PAD5)
+
+        self.cmnt_txt = ttk.Entry(c, textvariable=self.cmnt_var)
+        self.cmnt_txt.grid(row=2, column=1, sticky='ew')
+
+        self.gen_pop.grid_columnconfigure(0, weight=1)
+        c.grid_columnconfigure(1, weight=1)
+
+        self.gen_ok_btn.grid(row=0, column=0)
+        cancel = ttk.Button(bb, text='cancel', command=self.gen_pop.destroy)
+        cancel.grid(row=0, column=1)
 
     def proc_gen_key(self):
+        name = self.name_var.get()
+        email = self.email_var.get()
+        comment = self.cmnt_var.get()
+
+        key = KeyStuff.generate_key(name, email, comment=comment, protect=True)
+        # add key to user's local database
+        DBStuff.insert_key(key)
+
         self.gen_pop.destroy()
 
     def run(self):
-        self.win.mainloop()
+        try:
+            self.win.mainloop()
+        except KeyboardInterrupt:
+            self.shutdown()
 
-    def upload_key(self, option=PGPKeys.PUB):
-        pass
+    def validate_gen_key(self, evt: tk.Event = None):
+        name = self.name_var.get()
+        email = self.email_var.get()
+        comment = self.cmnt_var.get()
+        print(f"{name}; {email}; {comment}")
+
+        is_name_valid = len(name.strip()) >= 3
+        is_email_valid = re.match(
+            r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?",
+            email.strip())
+
+        if is_email_valid and is_name_valid:
+            self.gen_ok_btn.configure(state=tk.NORMAL)
+        else:
+            self.gen_ok_btn.configure(state=tk.DISABLED)
 
 
 def main():
